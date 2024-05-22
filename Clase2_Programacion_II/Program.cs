8101ef57b0e76e27ba938b90fa47c511a56fff94
Leet36 <ctinoco@edu.upolitecnica.cr>
2024-05-21 20:51:20 -0600

    Agregar archivos de proyecto.

---
+++ Clase2_Programacion_II/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2_Programacion_II
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string apellido = "";
+            Console.WriteLine("Ingrese apellido");
+            apellido=Console.ReadLine();
+            Console.WriteLine(apellido);
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
